8e57e0ae765dcd535d5f43baf563bb0a0b294cb3
Abhishek_00700 <abhishek.kumar.38.4@mountblue.tech>
2026-09-08 22:42:41 +0530

    fix

--- marble_project/marble-admin-frontend/src/services/authService.ts
+++ marble_project/marble-admin-frontend/src/services/authService.ts
@@ -4,8 +4,7 @@ import type {
   LoginCredentials,
 } from '@/types'
 
-//const API_URL = 'http://localhost:8080/api'
-const API_URL = `${import.meta.env.VITE_API_BASE_URL}/admins`
+const API_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8080/api'
 const STORAGE_KEYS = {
   user: 'admin_user',
   tokens: 'admin_tokens',
